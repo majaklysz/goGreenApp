bb660a306cccf84fe2c645378728a8559c4e60ae
maja <eaamkly@students.eaaa.dk>
2023-12-02 11:23:21 +0100

    login no use

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,5 +1,31 @@
-import React from "react";
-
+import "../login/Login.css";
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <section>
+      <div className="pageAuth">
+        <img
+          className="logo"
+          src="src\assets\logo2.svg"
+          alt="logo greenclean"
+        />
+        <p className="underLogo">
+          Join now for a cleaner, greener home and embrace sustainable living
+          effortlessly with our app!
+        </p>
+        <h1>Login</h1>
+        <form action="" className="formLogin">
+          <input type="email" name="mail" placeholder="Type your mail" />
+          <input
+            type="password"
+            name="password"
+            placeholder="Type your password"
+          />
+          <button className="ctaA">
+            Login <img src="src\assets\fi-rr-angle-right.svg" alt="arrow" />
+          </button>
+        </form>
+        <p className="text-center">Dont have an account?Sign Up</p>
+      </div>
+    </section>
+  );
 }
